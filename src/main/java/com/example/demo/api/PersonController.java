579c96f758e9a3c08e0f3c74b9e843eedaa2db56
José Ángel <jose.a.l.e@hotmail.com>
2023-05-01 11:11:06 +0200

    Add initial API endpoints

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,12 +3,16 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * PersonController class represents a controller for managing Person objects.
  * It is responsible for adding new Person objects through the PersonService.
  */
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
 
@@ -28,7 +32,8 @@ public class PersonController {
      * Adds a new Person using PersonService object.
      * @param person New Person to be created.
      */
-    public void addPerson(Person person) {
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
     }
 }
--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
 /**
@@ -19,7 +21,7 @@ public class Person {
      * @param id Unique ID of the person.
      * @param name Name of the person.
      */
-    public Person(UUID id, String name) {
+    public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
         this.id = id;
         this.name = name;
     }
